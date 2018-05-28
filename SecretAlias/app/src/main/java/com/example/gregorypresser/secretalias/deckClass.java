fe7e521eb3a0f526cffc600840f7829b5968acf1
mosesb123 <mosesbakst@gmail.com>
2018-05-28 01:30:05 -0400

    Deck maker

--- SecretAlias/app/src/main/java/com/example/gregorypresser/secretalias/deckClass.java
+++ SecretAlias/app/src/main/java/com/example/gregorypresser/secretalias/deckClass.java
@@ -1,8 +1,9 @@
 package com.example.gregorypresser.secretalias;
 import java.lang.Math;
+import java.util.ArrayList;
 
 class Deck {
-    private String allAliases[] = new String[100]; //all the aliases we have
+    private ArrayList allAliases = new ArrayList; //all the aliases we have
     private Card currentDeck[][] = new Card[5][5]; //this specific game
 //amount of types per deck
     private int purples = 7;
@@ -18,15 +19,28 @@ class Deck {
     int cR = 0; //Current Row
     int cC = 0; //Current Column
     int todaysAliasIndex = 0;
+    int todaysAliasesRemaining = allAliases.size;
+    ArrayList todaysAliases  = allAliases.clone();
     for (int i = 0; i < 16; i++) {
         cR = (int)(Math.random() * 5);
         cC = (int)(Math.random() * 5);
+        todaysAliasIndex = (int)(Math.random() * todaysAliasesRemaining);
         if (currentDeck[cR][cC].getType().equals("passenger"))
             i--;
         else {
-            if (i == 0)
-                currentDeck[cR][cC] = new Card(todaysAlias, "spy");
+                currentDeck[cR][cC] = new Card(todaysAliases.get(todaysAliasIndex), typeSetter(i));
+                todaysAliasesRemaining--;
+                todaysAliases.remove(todaysAliasIndex);
+                
             }
         }
+    }
+
+    public typeSetter (int i) {
+        if (i == 0) 
+            return "spy";
+        else if (i < purples + 1)
+            return "purple";
+        return "green";
     }
 }
